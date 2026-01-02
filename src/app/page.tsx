c3773a7fdd2b5fccb48b57c5b807f3e38f6361d5
Leo <leonardo2002gonzalez@gmail.com>
2026-01-02 14:21:27 -0300

    Fix global 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Portfolio from "./Portfolio";
 import { getTrabajo, getSobreMi } from "../../lib/services/portfolio";
 
+export const revalidate = 0;
+
 export default async function Home() {
   const trabajo = await getTrabajo();
   const sobreMi = await getSobreMi();
